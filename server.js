8d71a8a457188660353bca99c469a1961508a760
aronweston <aron@firststepdigital.com.au>
2021-03-09 07:34:46 +1100

    server.js

--- server.js
+++ server.js
@@ -36,15 +36,7 @@ server.use('/order/', orderRoutes);
 server.use(notFound);
 server.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, function () {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env
-  );
-});
+server.listen(process.env.PORT);
 
 // //Port & Listen
 // server.listen(PORT);
