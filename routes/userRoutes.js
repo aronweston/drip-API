6722d4244be3f0d63e639c68d9f5f5441c1d5380
aronweston <aron@firststepdigital.com.au>
2021-03-09 09:51:02 +1100

    reset api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ import {
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 //PUBLIC
-router.route('/api/register').post(createUser);
+router.route('/register').post(createUser);
 router.post('/login', authUser);
 
 // PRIVATE
